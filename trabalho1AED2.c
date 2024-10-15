0886476a01bb8f3ae19a64c08b44008542e737d8
João Vitor Bufon de Almeida <129139731+JoaoBufon@users.noreply.github.com>
2024-10-15 00:04:06 -0300

    added binary archives and generate code for them

--- trabalho1AED2.c
+++ trabalho1AED2.c
@@ -17,11 +17,22 @@ typedef struct
     char category_code[N];
     char brand[N];
     float price;
-} DadoProduto;  
+} DadoProduto;
+
+typedef struct 
+{
+    int user_id;
+    char user_session [N];
+    char event_time[25];
+    char event_type[20];
+} DadoAcesso;
+
 
 void parseRawLineToDadosProdutos(char *line, DadoProduto *registroProduto){
     char *token;
 
+    memset(registroProduto->category_code, '\0', N);
+    memset(registroProduto->brand,'\0',N);
     // Skip event_time
     token = strtok(line, ",");
     
@@ -46,10 +57,53 @@ void parseRawLineToDadosProdutos(char *line, DadoProduto *registroProduto){
     registroProduto->price = atof(strtok(NULL, ","));
 }
 
+void parseRawLineToDadosAcesso(char *line, DadoAcesso *registroAcesso){
+    char *token;
+
+    memset(registroAcesso->event_time, '\0', 25);
+    memset(registroAcesso->event_type, '\0', 20);
+    memset(registroAcesso->user_session, '\0', N);
+
+    // Skip event_time
+    token = strtok(NULL, ",");
+    strncpy(registroAcesso->event_time, token ? token : "",  25);
+    
+    // Skip event_type
+    token = strtok(NULL, ",");
+    strncpy(registroAcesso->event_type, token ? token : "", 20);
+
+    // Extract product_id
+    strtok(NULL, ",");
+
+    // Extract category_id
+    strtok(NULL, ",");
+
+    // Extract category_code
+    strtok(NULL, ",");
+
+    // Extract brand
+    strtok(NULL, ",");
+
+    // Extract price
+    strtok(NULL, ",");
+
+    // Extract user_id
+    registroAcesso->user_id = atoi(strtok(NULL, ""));
+
+    // Extract user_session
+    token = strtok(NULL, ",");
+    strncpy(registroAcesso->user_session, token ? token : "", N);
+}
+
 void criarArquivosDeDados(){
-    FILE *csv_file = fopen("2019-Nov.csv", "r");
-    FILE *fDadosProdutos = fopen(dadosAcesso, "wb");
     char line[1024];
+    int cont = 0;
+
+    FILE *csv_file = fopen("2019-Nov.csv", "r");
+    FILE *fDadosProdutos = fopen(dadosProdutos, "wb");
+    FILE *fDadosAcessos = fopen(dadosAcesso, "wb");
+
+    DadoAcesso registroAcesso;
     DadoProduto registroProduto;
 
     if (csv_file == NULL){
@@ -65,16 +119,21 @@ void criarArquivosDeDados(){
     fgets(line, sizeof(line), csv_file);//ignorar o cabecalho
 
     printf("Começou leitura e inserção dos dados");
-    
+
     while(fgets(line, sizeof(line), csv_file)){
         line[strcspn(line, "\n")] = 0;
+        printf("%d", cont++);
         parseRawLineToDadosProdutos(line, &registroProduto);
+        parseRawLineToDadosAcesso(line, &registroAcesso);
+
+        fwrite(&registroAcesso, sizeof(registroAcesso),1,fDadosAcessos);
         fwrite(&registroProduto, sizeof(registroProduto),1,fDadosProdutos);
     }
 
     printf("Terminou leitura e inserção dos dados");
     fclose(csv_file);
     fclose(fDadosProdutos);
+    fclose(fDadosAcessos);
 }
 
 int main(){
